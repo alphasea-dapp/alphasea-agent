3dfd3b3c3bef2d5b7cc228056724e8a6bada31d2
a <a@example.com>
2022-01-17 10:49:27 +0900

    restrict model_id to lowercase

--- tests/predictor/test_validate_model_id.py
+++ tests/predictor/test_validate_model_id.py
@@ -17,9 +17,9 @@ from src.predictor.model_id import validate_model_id
 
 class TestPredictorValidateModelId(TestCase):
     def test_ok(self):
-        validate_model_id('_aA0')
-        validate_model_id('aA0_')
-        validate_model_id('A0_a')
+        validate_model_id('_az09')
+        validate_model_id('a' * 4)
+        validate_model_id('a' * 31)
 
     def test_too_short_error(self):
         with self.assertRaises(ValidationError):
